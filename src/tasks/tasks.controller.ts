6bd88936a1a2cd885e9f7db439077bd8419545cc
Sulabh Ghimire <sulabhghimire@pop-os.localdomain>
2024-03-11 22:47:51 +0545

    [Feature] Task Module without data presistance

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,43 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-task-filter.dto';
+import { filter } from 'rxjs';
+
+@Controller('tasks')
+export class TasksController {
+
+    constructor(private taskService: TasksService) { }
+
+    @Get()
+    getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+        if (Object.keys(filterDto).length)
+            return this.taskService.getTasksWithFilters(filterDto);
+        else
+            return this.taskService.getAllTasks();
+    }
+
+    @Get('/:id')
+    getTaskById(@Param() id: string): Task {
+        return this.taskService.getTaskById(id);
+    }
+
+    @Post()
+    createTask(@Body() createTaskDto: CreateTaskDto): Task {
+        return this.taskService.createTask(
+            createTaskDto
+        );
+    }
+
+    @Delete('/:id')
+    deleteTaskById(@Param('id') id: string) {
+        this.taskService.deleteTaskById(id);
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(@Param('id') id: string, @Body('status') status: TaskStatus): Task {
+        return this.taskService.updateTaskStatus(id, status);
+    }
+
+}
